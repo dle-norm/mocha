48701a1c74ecb1524e86332f7f1ee77a2ea0984b
dlenormand <dlenormand19@gmail.com>
2022-08-09 19:41:22 +0200

    feat: html skeleton

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -3,11 +3,63 @@ const host = 'localhost';
 const port = 8000;
 const fs = require('fs').promises;
 const requestListener = function (req, res) {
-    if (req.url !== "/index.html") {
-        // redirect on 404
-        fs.readFile(__dirname + `/../404.html`).then(contents => {
+    const whitelist = [
+        '/scripts/index.js',
+        '/css/index.css',
+        '/index.html'
+    ];
+    const images = [
+        '/assets/mochacafe/Ressources/logo.jpg',
+        '/assets/mochacafe/Ressources/Cafe-En-Grain.jpg',
+        '/assets/mochacafe/Ressources/150929101049-black-coffee-stock-exlarge-169.jpg',
+        '/assets/mochacafe/Ressources/cockroach-coffee-art-by-chang_1.jpg',
+        '/assets/mochacafe/Ressources/Chocolate-Marshmallow-Cooki.jpg',
+        '/assets/mochacafe/Ressources/HOPE-Coffee-@-DTS-2017-600x300.jpg',
+    ];
+    if (whitelist.includes(req.url)) { 
+        fs.readFile(__dirname + `/../${req.url}`).then(contents => {
             res.setHeader("Content-Type", "text/html");
-            res.writeHead(404);
+            res.writeHead(200);
+            res.end(contents);
+        }).catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+    } else if (images.includes(req.url)) { 
+        fs.readFile(__dirname + `/../${req.url}`).then(contents => {
+            res.setHeader("Content-Type", "image/jpg");
+            res.writeHead(200);
+            res.end(contents);
+        }).catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+    } else if (req.url === "/") {
+        fs.readFile(__dirname + `/../index.html`).then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        }).catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+    } else if (req.url === "/sitemap.xml") {
+        fs.readFile(__dirname + `/../sitemap.xml`).then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        }).catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+    } else if (req.url === "/robots.txt") {
+        fs.readFile(__dirname + `/../robots.txt`).then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
             res.end(contents);
         }).catch(err => {
             res.writeHead(500);
@@ -15,9 +67,10 @@ const requestListener = function (req, res) {
             return;
         });
     } else {
-        fs.readFile(__dirname + `/../index.html`).then(contents => {
+        // redirect on 404
+        fs.readFile(__dirname + `/../404.html`).then(contents => {
             res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
+            res.writeHead(404);
             res.end(contents);
         }).catch(err => {
             res.writeHead(500);
--- test/test.mjs
+++ test/test.mjs
@@ -1,14 +1,40 @@
 import fetch from 'node-fetch';
 
 // test if the redirection to 404 is operationnal.
-const url = "http://localhost:8000/badUri";
+let url = "http://localhost:8000/badUri";
 fetch(url)
   .then( res => res.text() )
   .then( data => { 
-    const notFound = `<!doctype html><html lang="fr">    <head>        <meta charset="utf-8">        <title>404 NOT FOUND</title>        <link rel="stylesheet" href="./css/404.css">        <script src="./scripts/404.js"></script>    </head>    <body>        <p>En construction</p>        <a href="./index.html">            Retourner à l'accueil        </a>    </body></html>`;
+    const notFound = `<!doctype html><html lang="fr">    <head>        <meta charset="utf-8">        <title>404 NOT FOUND</title>        <link rel="stylesheet" href="./css/404.css">        <script src="./scripts/404.js"></script>    </head>    <body>        <p>En construction</p>        <a href="/">            Retourner à l'accueil        </a>    </body></html>`;
     if (data.replace(/[\n\r]+/g, '').trim() === notFound) {
-      console.log('All good !!!');
+      console.log('404 good !!!');
     } else {
       console.log('Errors 404 test.');
+    } 
+});
+
+// test if the robots.txt is operationnal.
+url = "http://localhost:8000/robots.txt";
+fetch(url)
+  .then( res => res.text() )
+  .then( data => {
+    const notFound = `User-agent: *Disallow: /assetsDisallow: /scriptsDisallow: /css`;
+    if (data.replace(/[\n\r]+/g, '').trim() === notFound) {
+      console.log('robots.txt good !!!');
+    } else {
+      console.log('Errors robots.txt test.');
+    } 
+});
+
+// test if the sitemap.xml is operationnal.
+url = "http://localhost:8000/sitemap.xml";
+fetch(url)
+  .then( res => res.text() )
+  .then( data => { 
+    const notFound = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">  <url>    <loc>https://moca-cafe.herokuapp.com</loc>    <lastmod>2022-08-09</lastmod>  </url></urlset>`;
+    if (data.replace(/[\n\r]+/g, '').trim() === notFound) {
+      console.log('sitemap.xml good !!!');
+    } else {
+      console.log('Errors sitemap.xml test.');
     } 
 });
